218e59f6dc84e93548a168cfa79eaea72f415590
shashank <subedishashank2@gmail.com>
2022-07-18 17:44:18 +0545

    final output

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const logo = document.querySelector(".logo");
 const all = document.querySelector("*");
 const header = document.querySelector("header");
 const hero = document.getElementById("hero");
+const hero__header = document.getElementById("hero__header");
 const hero__body = document.getElementById("hero__body");
 const hero__img = document.getElementById("hero__ImgDiv");
 const tiles = document.getElementById("tiles");
@@ -12,6 +13,9 @@ const benefits = document.getElementById("benefits");
 const founder = document.getElementById("founder");
 const gallery = document.getElementById("gallery");
 const footer = document.getElementById("footer");
+const navbutn = document.getElementById("navbtn");
+const heronav__body = document.getElementById("hero__navbody");
+const nav__subtwo = document.getElementById("nav__subtwo");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
@@ -28,4 +32,15 @@ hamburger.addEventListener("click", () => {
   founder.classList.toggle("active");
   footer.classList.toggle("active");
   gallery.classList.toggle("active");
+});
+navbutn.addEventListener("click", () => {
+  if (window.visualViewport.width > 950) {
+    hero__body.style.display = "none";
+    heronav__body.style.display = "block";
+    hero.style.height = "45rem";
+    hero__img.style.display = "none";
+    tiles.style.marginTop = "2rem";
+    hero__header.style.paddingBottom = "0";
+    hero__header.style.borderBottom = "1px solid #C4C4C4";
+  }
 });
