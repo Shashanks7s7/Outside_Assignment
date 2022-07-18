c961ab2d23d5306975ecc7b3dbbfd6fc50f0ff10
shashank <subedishashank2@gmail.com>
2022-07-18 08:24:00 +0545

    burger menu

--- js/script.js
+++ js/script.js
@@ -1,20 +1,31 @@
-const hamburger=document.querySelector(".hamburger");
-const navLinks=document.querySelector(".nav__links");
-const logo=document.querySelector(".logo");
-const all=document.querySelector("*");
-const header=document.querySelector("header");
-const hero=document.getElementById("hero")
-const hero__body=document.getElementById("hero__body");
-const hero__img=document.getElementById("hero__ImgDiv")
-console.log(hero__body);
-hamburger.addEventListener("click",()=>{
-    hamburger.classList.toggle("active");
-    navLinks.classList.toggle("active");
-    logo.classList.toggle("active");
-    all.classList.toggle("active");
-    header.classList.toggle("active");
-    hero.classList.toggle("active");
-    hero__body.classList.toggle("active");
-hero__img.classList.toggle("active")
-    console.log("hehe");
-})
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav__links");
+const logo = document.querySelector(".logo");
+const all = document.querySelector("*");
+const header = document.querySelector("header");
+const hero = document.getElementById("hero");
+const hero__body = document.getElementById("hero__body");
+const hero__img = document.getElementById("hero__ImgDiv");
+const tiles = document.getElementById("tiles");
+const mission = document.getElementById("mission");
+const benefits = document.getElementById("benefits");
+const founder = document.getElementById("founder");
+const gallery = document.getElementById("gallery");
+const footer = document.getElementById("footer");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navLinks.classList.toggle("active");
+  logo.classList.toggle("active");
+  all.classList.toggle("active");
+  header.classList.toggle("active");
+  hero.classList.toggle("active");
+  hero__body.classList.toggle("active");
+  hero__img.classList.toggle("active");
+  tiles.classList.toggle("active");
+  mission.classList.toggle("active");
+  benefits.classList.toggle("active");
+  founder.classList.toggle("active");
+  footer.classList.toggle("active");
+  gallery.classList.toggle("active");
+});
